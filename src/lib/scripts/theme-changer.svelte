83940461af3a3e243fd81fe8caf45059088b3fef
RayRaivern <ray456654@gmail.com>
2025-12-22 02:54:25 +0600

    Added transition between color changes by using CSS transition inside theme-changer.

--- src/lib/scripts/theme-changer.svelte
+++ src/lib/scripts/theme-changer.svelte
@@ -6,3 +6,11 @@
   const theme = $derived(themeFromSourceColor(argbFromHex(color)));
   $effect(() => applyTheme(theme, {target: document.body, dark: true}));
 </script>
+
+<style>
+  :global(*) {
+    transition: background-color 1.2s ease-out,
+                border-color 1.2s linear,
+                color 1.2s ease-in;
+  }
+</style>
