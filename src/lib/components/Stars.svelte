9363b1abdad817ecf79752cf91d0c961e2f6905f
RayRaivern <ray456654@gmail.com>
2026-06-11 03:22:37 +0600

    The stars move now.

--- src/lib/components/Stars.svelte
+++ src/lib/components/Stars.svelte
@@ -23,16 +23,24 @@
 		const ctx = canvas.getContext('2d');
 
 		let starCount = window.innerWidth * 0.2;
-		const stars = starGenerator(starCount, 1.5, width, height);
+		const stars = starGenerator(starCount, 1, width, height);
 		let animationID: number;
+
+		const move_speed = 0.01;
+    const parallax = 0.04;
 
 		function animate(time: number) {
 			if (!ctx) throw new Error('Failed to get canvas 2d context for background Stars.');
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 			stars.forEach((star) => {
+				star.x += move_speed + (star.radius * parallax);
+				if (star.x - star.radius > width) {
+					star.x = -star.radius;
+				}
+
 				const twinkle = (Math.sin(time * 0.0008 + star.phase) + 1) * 0.5;
-				const minOpacity = star.opacity * 0; // 0.[value]% of max as minimum
+				const minOpacity = star.opacity * 0.3; // 0.[value]% of max as minimum
 				const opacity = minOpacity + twinkle * (star.opacity - minOpacity);
 
 				ctx.fillStyle = star.color;
